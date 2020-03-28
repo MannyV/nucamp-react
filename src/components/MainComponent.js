d70be1c50ebb2f5cf9bbe0948ab90e75e98102f5
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-03-28 10:16:06 -0400

    Added About Us

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import { CAMPSITES } from "../shared/campsites"
 import Header from "./HeaderComponent"
 import Footer from "./FooterComponent"
 import Home from "./HomeComponent"
+import About from "./AboutComponent"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { COMMENTS } from "../shared/comments"
 import { PARTNERS } from "../shared/partners"
@@ -65,8 +66,13 @@ class Main extends Component {
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Redirect to="/home" />
         </Switch>
+
         <Footer />
       </div>
     )
