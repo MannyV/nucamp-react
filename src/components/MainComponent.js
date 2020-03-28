d1f41e6be47f49ceb922eb1ecb3a3c64fb0139d3
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-03-27 21:58:43 -0400

    Working version mid route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,36 +3,38 @@ import Directory from "./DirectoryComponent";
 import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 import CampsiteInfo from "./CampsiteInfoComponents";
+import Contact from './ContactComponent';
+
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: CAMPSITES,
-      selectedCampsite: null
+      campsites: CAMPSITES
     };
   }
 
-  onCampsiteSelect(campsiteId) {
-    this.setState({ selectedCampsite: campsiteId });
-  }
-
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
     return (
       <div>
         <Header />
-        <Directory
-          campsites={this.state.campsites}
-          onClick={campsiteId => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              campsite => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
+
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
